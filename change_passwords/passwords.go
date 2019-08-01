132153f1b6e8d32424da034e34cbfecb429314ff
savage_floweed <FloydQaranja@gmail.com>
2019-08-01 12:56:34 +0300

    cleaning up code since am importing it.

--- change_passwords/passwords.go
+++ change_passwords/passwords.go
@@ -15,38 +15,32 @@ func main() {
 	password := os.Args[1] //getting new password to set
 
 	db, err := sql.Open("mysql", "user:password@tcp(127.0.0.1:3306)/mysql") //git
-	logger(err)
 	utilities.Logger(err)
 	defer db.Close()
 
 	rows, err := db.Query("select user from user where user not in ('some_user', 'some_other_user') and host = '%' order by user desc;")
-	logger(err)
+	utilities.Logger(err)
 	defer rows.Close()
 
 	tx, err := db.Begin()
-	logger(err)
+	utilities.Logger(err)
 	defer tx.Rollback()
 
 	var username string
 
 	for rows.Next() {
 		err := rows.Scan(&username)
-		logger(err)
+		utilities.Logger(err)
 
 		// updatestmt, err := tx.Prepare("set password for ? = ?")
 		// logger(err)
 		// _, err = updatestmt.Exec(username, password)
 		_, err = tx.Exec("set password for '" + username + "' = '" + password + "'")
-		logger(err)
+		utilities.Logger(err)
 		log.Println("User " + username + " processed.")
 	}
 	err = rows.Err()
-	logger(err)
+	utilities.Logger(err)
 	err = tx.Commit()
-	logger(err)
-}
-func logger(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
+	utilities.Logger(err)
 }
