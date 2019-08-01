924e0c1a56073114dd7ded2d5c80b045094b9cec
savage_floweed <FloydQaranja@gmail.com>
2019-08-01 11:32:51 +0300

    Initial commit of project(cliche meassage :D)

---
+++ src/change_passwords/passwords.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var password string
+
+func main() {
+	password := os.Args[1] //getting new password to set
+
+	db, err := sql.Open("mysql", "user:password@tcp(127.0.0.1:3306)/mysql") //git
+	logger(err)
+	defer db.Close()
+
+	rows, err := db.Query("select user from user where user not in ('massa2014', 'root', 'mysql.session','mysql.sys', 'KFK_IT', 'KFK_IT', 'KFK_Replicator2', 'nwrvam', 'chartio') and host = '%' order by user desc;")
+	logger(err)
+	defer rows.Close()
+
+	tx, err := db.Begin()
+	logger(err)
+	defer tx.Rollback()
+
+	var username string
+
+	for rows.Next() {
+		err := rows.Scan(&username)
+		logger(err)
+
+		// updatestmt, err := tx.Prepare("set password for ? = ?")
+		// logger(err)
+		// _, err = updatestmt.Exec(username, password)
+		_, err = tx.Exec("set password for '" + username + "' = '" + password + "'")
+		logger(err)
+		log.Println("User " + username + " processed.")
+	}
+	err = rows.Err()
+	logger(err)
+	err = tx.Commit()
+	logger(err)
+}
+func logger(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
